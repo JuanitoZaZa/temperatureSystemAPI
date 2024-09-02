8050e2bf2285e3e498bea3c8ab7d303287a0edcb
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-02 16:24:17 -0500

    first commit

---
+++ src/main/java/com/TemperatureSystem/TemperatureSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.TemperatureSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TemperatureSystem/TemperatureSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TemperatureSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
